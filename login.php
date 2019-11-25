b70a3dc7f169a2c79312321fec2d561168578d25
Bryant <bryant@ternio.io>
2019-11-24 23:22:42 -0500

    login page

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+
+<title>Pixelcut Login</title> 
+
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="responsive.css" />
+
+<!-- ROBOTO GOOG FONT -->
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<!-- OPEN ASNS GOOG FONT -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="logo-wrapper">
+	<div class="logo-margin">
+		<div class="login-logo"></div>
+		<div class="login-box">
+			<div class="login-welcome">
+				Welcome back!<br>
+				Log into your account below:
+			</div>
+			<form class="login-formarea">
+				<div>
+					<label>EMAIL ADDRESS</label>
+					<input type="text">
+					<span class="login-at"></span>
+				</div>
+				<div>
+					<label>PASSWORD</label>
+					<span class="login-forgotpass">forgot password?</span>
+					<input type="password">
+					<span class="login-lock"></span>
+				</div>
+				<div>
+					<submit>SIGN IN</submit>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
