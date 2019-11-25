b1573c80cef4b76a5f61438b5cf11412207670a3
Bryant <bryant@ternio.io>
2019-11-24 23:43:23 -0500

    forgot + setup pass

---
+++ forgot-pass.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+
+<title>Pixelcut Forgot Password</title> 
+
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="responsive.css" />
+
+<!-- ROBOTO GOOG FONT -->
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<!-- OPEN ASNS GOOG FONT -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="logo-wrapper">
+	<div class="logo-margin">
+		<div class="login-logo"></div>
+		<div class="login-box forgotpass-box">
+			<div class="login-welcome">
+				Forgot your password?<br>
+				Fill in your email below to reset your password.
+			</div>
+			<form class="login-formarea">
+				<div>
+					<label>EMAIL ADDRESS</label>
+					<input type="text">
+					<span class="login-at"></span>
+				</div>
+				<div>
+					<submit>RESET</submit>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
---
+++ setup-pass.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+
+<title>Pixelcut Setup Password</title> 
+
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="responsive.css" />
+
+<!-- ROBOTO GOOG FONT -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
+<!-- OPEN SANS GOOG FONT -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="logo-wrapper">
+	<div class="logo-margin">
+		<div class="login-logo"></div>
+		<div class="login-box setuppass-box">
+			<div class="setuppass-title">Let’s Get This Party Started 🎉</div>
+			<div class="login-welcome setuppass-welcome">Content is vital for SEO. We’ve built a dashboard<br>that streamlines the content creation process. All you need to do to get started is set a password.</div>
+			<form class="login-formarea">
+				<div>
+					<label>YOUR EMAIL ADDRESS</label>
+					<input type="text" value="email@email.com" disabled>
+				</div>
+				<div>
+					<label>SETUP YOUR PASSWORD</label>
+					<input type="password">
+					<span class="login-lock"></span>
+				</div>
+				<div>
+					<label>CONFIRM YOUR PASSWORD</label>
+					<input type="password">
+					<span class="login-lock"></span>
+				</div>
+				<div>
+					<submit>SIGN IN</submit>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
